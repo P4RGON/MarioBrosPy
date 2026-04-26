49acd195cc52af330f3dbc5faf5d70188f9f8361
P4RGON <pargon2001@hotmail.com>
2026-04-26 20:34:59 +0200

    Add: Project with classes and assets

---
+++ MarioBrosPy_Project/Scripts/Pesonajes.py
@@ -0,0 +1,6 @@
+class Pesonajes:
+    def __init__(self,nombre, tecla_up, tecla_down, floor):
+        self.nombre = nombre
+        self.mov_up = tecla_up
+        self.mov_down = tecla_down
+        self.floor = floor
